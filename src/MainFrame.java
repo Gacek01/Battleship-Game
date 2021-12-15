4ccf836464f40d579ab690aa5abc964650d56b8b
Krzysztof Gagacki <krzysztofgagacki@gmail.com>
2021-12-15 21:23:23 +0100

    created ship placement

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,12 +1,14 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
+
 import javax.swing.*;
 
 public class MainFrame extends JFrame implements ActionListener {
 
 	public final int UNIT_SIZE = 50;
-	boolean[][] board = new boolean[10][10];
+	byte[][] board = new byte[10][10];
 	JButton[] ships = new JButton[100];
 	JLabel[] labels = new JLabel[10];
 	JLabel shotsLabel = new JLabel("0");
@@ -14,12 +16,8 @@ public class MainFrame extends JFrame implements ActionListener {
 	JProgressBar progressBar = new JProgressBar(0, 20);
 	byte shipsSunk = 0;
 	byte shotsFired = 0;
-	
-	MainFrame() {
 
-		// Test data
-		board[0][0] = true;
-		board[9][9] = true;
+	MainFrame() {
 
 		/* ------------Top axis setup-------------- */
 		JPanel topPanel = new JPanel();
@@ -40,7 +38,9 @@ public class MainFrame extends JFrame implements ActionListener {
 		progressBar.setFont(mainFont);
 		progressBar.setPreferredSize(new Dimension(500, 100));
 		progressBar.setForeground(Color.red);
+		progressBar.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
 		shotsLabel.setFont(mainFont);
+		bottomPanel.add(new JLabel("Shots fired: ")).setFont(mainFont);
 		bottomPanel.add(shotsLabel);
 		bottomPanel.add(progressBar);
 
@@ -54,16 +54,17 @@ public class MainFrame extends JFrame implements ActionListener {
 			ships[i].addActionListener(this);
 			ships[i].setFocusable(false);
 			centerPanel.add(ships[i]);
-
 		}
+		placeShips();
+		drawTestBoard(board);
 
 		/* ------------JFrame setup-------------- */
 		this.setIconImage(new ImageIcon("src//ship.png").getImage());
-		this.setSize(515, 800);
+		this.setSize(515, 750);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 		this.setLayout(null);
-		this.setTitle("Battleship");
+		this.setTitle("+++Battleship+++");
 		this.setLocationRelativeTo(null);
 		this.setResizable(false);
 		this.add(topPanel);
@@ -80,9 +81,22 @@ public class MainFrame extends JFrame implements ActionListener {
 				ships[i].setEnabled(false);
 				shotsLabel.setText(String.valueOf(shotsFired));
 				System.out.print("Shot to " + ((int) i / 10 + 1) + " - " + ((int) i % 10 + 1) + " ... ");
-				if (checkIfShipHit((int) i / 10, (int) i % 10)) {
+				if (checkIfShipHit((int) i / 10, (int) i % 10) > 0) {
 					ships[i].setText("+");
-					ships[i].setBackground(Color.red);
+					switch (checkIfShipHit((int) i / 10, (int) i % 10)) {
+					case 4:
+						ships[i].setBackground(Color.pink);
+						break;
+					case 3:
+						ships[i].setBackground(Color.yellow);
+						break;
+					case 2:
+						ships[i].setBackground(Color.orange);
+						break;
+					case 1:
+						ships[i].setBackground(Color.green);
+						break;
+					}
 					System.out.println("Hit!");
 					progressBar.setValue(progressBar.getValue() + 1);
 				} else {
@@ -94,12 +108,105 @@ public class MainFrame extends JFrame implements ActionListener {
 		}
 	}
 
-	public boolean checkIfShipHit(int x, int y) {
+	public byte checkIfShipHit(int x, int y) {
 		return board[x][y];
 	}
 
 	public boolean checkIfAllShipsSunk() {
 		return shipsSunk == 20;
+	}
+
+	public void drawTestBoard(byte[][] board) {
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board.length; j++) {
+				System.out.print(board[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public void placeShips() {
+		Random random = new Random();
+		int x, y, dir, counter = 0;
+
+		/* place 1x 4-square ship */
+		while (counter != 1) {
+			dir = random.nextInt(2); // 0-place to right, 1-place down
+			x = random.nextInt(7);
+			y = random.nextInt(7);
+			if (dir == 0) {
+				if (board[x][y] == 0 && board[x][y + 1] == 0 && board[x][y + 2] == 0 && board[x][y + 3] == 0) {
+					board[x][y] = 4;
+					board[x][y + 1] = 4;
+					board[x][y + 2] = 4;
+					board[x][y + 3] = 4;
+					counter++;
+				}
+			} else {
+				if (board[x][y] == 0 && board[x + 1][y] == 0 && board[x + 2][y] == 0 && board[x + 3][y] == 0) {
+					board[x][y] = 4;
+					board[x + 1][y] = 4;
+					board[x + 2][y] = 4;
+					board[x + 3][y] = 4;
+					counter++;
+				}
+			}
+		}
+
+		/* place 2x 3-square ships */
+		counter = 0;
+		while (counter != 2) {
+			dir = random.nextInt(2); // 0-place to right, 1-place down
+			x = random.nextInt(8);
+			y = random.nextInt(8);
+			if (dir == 0) {
+				if (board[x][y] == 0 && board[x][y + 1] == 0 && board[x][y + 2] == 0) {
+					board[x][y] = 3;
+					board[x][y + 1] = 3;
+					board[x][y + 2] = 3;
+					counter++;
+				}
+			} else {
+				if (board[x][y] == 0 && board[x + 1][y] == 0 && board[x + 2][y] == 0) {
+					board[x][y] = 3;
+					board[x + 1][y] = 3;
+					board[x + 2][y] = 3;
+					counter++;
+				}
+			}
+		}
+
+		/* place 3x 2-square ships */
+		counter = 0;
+		while (counter != 3) {
+			dir = random.nextInt(2); // 0-place to right, 1-place down
+			x = random.nextInt(9);
+			y = random.nextInt(9);
+			if (dir == 0) {
+				if (board[x][y] == 0 && board[x][y + 1] == 0) {
+					board[x][y] = 2;
+					board[x][y + 1] = 2;
+					counter++;
+				}
+			} else {
+				if (board[x][y] == 0 && board[x + 1][y] == 0) {
+					board[x][y] = 2;
+					board[x + 1][y] = 2;
+					counter++;
+				}
+			}
+		}
+
+		/* place 4x 1-square ships */
+		counter = 0;
+		while (counter != 4) {
+			x = random.nextInt(10);
+			y = random.nextInt(10);
+			if (board[x][y] == 0) {
+				board[x][y] = 1;
+				counter++;
+			}
+		}
 	}
 
 }
